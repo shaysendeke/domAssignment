0394ba2c8695c668ed9e2d0ab193f399b2436ac8
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-08-18 00:27:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// !!1
+// const button = document.getElementById("btn");
+// button.addEventListener ("click", function() {
+//     button.innerText = "please click on me"
+// })
+
+// !!2 
+// const first = document.getElementById("text1")
+// first.addEventListener ("click" ,function () {
+//     alert("2pac is still alive")
+// })
+
+// const second = document.getElementById("text2")
+// second.addEventListener ("click", function() {
+//     alert("what's going on?")
+// })
+
+// const third = document.getElementById("text3")
+// third.addEventListener ("click", function() {
+//     alert ("this is America")
+// })
+
+// const fourth = document.getElementById("text4")
+// fourth.addEventListener ("click", function(){
+//     alert ("YO! can I get some change?")
+// })
+
+
+// !!3
+// const ol = document.getElementById("songtable")
+// let li= document.getElementsByClassName("songList")
+// console.log(ol)
+// for (let i=0; i<li.length; i++){
+// li[i].addEventListener ("click", function(){
+//     console.log(li[i]);
+// })
+// }
+
+// !!4
+// const button = document.getElementById("paragraphButton")
+// let paragraph = document.getElementById("para")
+// let num = 0
+// while (button) {
+//     button.addEventListener("click", function(){
+//         paragraph.innerText = num++;
+//     })
+// break;
+// }
+
+// !!5
+// const buttonSecond = document.getElementById("lower")
+// let num2 = 0
+// while (buttonSecond) {
+//     buttonSecond.addEventListener("click", function(){
+//         paragraph.innerText = num--;
+//     })
+// break;
+// }
